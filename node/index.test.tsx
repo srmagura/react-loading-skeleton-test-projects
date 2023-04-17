57f3ed601cbf95dfa487e41fd2bb3929082570a8
Sam Magura <srmagura@gmail.com>
2023-04-17 06:09:15 -0400

    stuff

--- node/index.test.tsx
+++ node/index.test.tsx
@@ -1,7 +1,29 @@
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
 import Skeleton, {
   SkeletonTheme,
   SkeletonProps,
   SkeletonThemeProps,
 } from 'react-loading-skeleton';
 
-const s = Skeleton({});
+it('works', () => {
+  render(
+    <SkeletonTheme borderRadius={7}>
+      <Skeleton count={5} />
+    </SkeletonTheme>
+  );
+
+  const skeletons = Array.from(
+    document.querySelectorAll('.react-loading-skeleton')
+  );
+
+  expect(skeletons).toHaveLength(5);
+
+  for (const skeleton of skeletons) {
+    expect(skeleton).toBeVisible();
+    expect(skeleton).toHaveStyle({ borderRadius: '7px' });
+  }
+
+  const skeletonProps: SkeletonProps = { count: 1 };
+  const skeletonThemeProps: SkeletonThemeProps = { highlightColor: 'black' };
+});
