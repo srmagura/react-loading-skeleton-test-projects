526d268b31f98bc2ea664eccded4013b61edf831
Sam Magura <srmagura@gmail.com>
2023-04-29 19:28:11 -0400

    nextjs example done

---
+++ nextjs/src/app/page.tsx
@@ -0,0 +1,10 @@
+import '@srmagura/react-loading-skeleton/dist/skeleton.css';
+import Skeleton from '@srmagura/react-loading-skeleton';
+
+export default function Home() {
+  return (
+    <main>
+      <Skeleton highlightColor="purple" />
+    </main>
+  );
+}
