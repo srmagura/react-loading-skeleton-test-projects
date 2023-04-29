8140485263fbe4cb44e304ecfe737871952ec70d
Sam Magura <srmagura@gmail.com>
2023-04-29 19:34:29 -0400

    jest works

---
+++ node-cjs/main.test.ts
@@ -0,0 +1,6 @@
+import Skeleton, { SkeletonTheme } from '@srmagura/react-loading-skeleton';
+
+it('works', () => {
+  expect(typeof Skeleton).toBe('function');
+  expect(typeof SkeletonTheme).toBe('function');
+});
