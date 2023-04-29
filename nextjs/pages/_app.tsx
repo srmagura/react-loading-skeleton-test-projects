191c44c0cef5e5eed07ceab69db95cf437c772eb
Sam Magura <srmagura@gmail.com>
2023-04-29 19:23:24 -0400

    delete old projects

--- nextjs/pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import '../styles/globals.css'
-import '@srmagura/react-loading-skeleton/dist/skeleton.css'
-import type { AppProps } from 'next/app'
-
-function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />
-}
-export default MyApp
